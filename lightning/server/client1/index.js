14f61310ef876c18ff6529fb89d5bfc5aaba4a79
Jayvir Singh <610039964@musd.net>
2018-02-13 20:32:14 -0800

    update

--- lightning/server/client1/index.js
+++
@@ -1,6 +0,0 @@
-server.login("jayvirs1@gmail.com","password");
-server.onreceive = function(snapshot) {
-    if(commands[snapshot.val().Code.Command] !== undefined) {
-        commands[snapshot.val().Code.Command][2](snapshot);
-    }
-};
