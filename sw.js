7c9dc0e4032bace968c1fe2fad61d27dc0740e20
Jayvir Singh <610039964@musd.net>
2018-02-14 19:21:14 -0800

    update

--- sw.js
+++ sw.js
@@ -1,28 +1,9 @@
-importScripts("https://jayvir101.github.io/lightning/Games.js");
-var CACHE_NAME = 'cache-v2';
-
-var REQUIRED_FILES = [
-    "https://jayvir101.github.io/lightning-resources/offline.html",
-    "https://jayvir101.github.io/lightning/",
-    "https://jayvir101.github.io/lightning/index.html",
-    "https://jayvir101.github.io/lightning/web_light.js",
-    "https://jayvir101.github.io/lightning/server.js",
-    "https://jayvir101.github.io/lightning/index.css",
-    "https://jayvir101.github.io/lightning/Games.js",
-    "https://jayvir101.github.io/lightning/index.js",
-    "https://jayvir101.github.io/lightning/Settings.js",
-    "https://jayvir101.github.io/lightning/Settings/",
-    "https://jayvir101.github.io/lightning/Settings/index.html",
-    "https://jayvir101.github.io/lightning/Settings/index.js",
-    "https://jayvir101.github.io/lightning/Games/index.js",
-    "https://jayvir101.github.io/lightning/Games/",
-    "https://jayvir101.github.io/lightning/Games/index.html",
-    "https://jayvir101.github.io/lightning-resources/landscape.png",
-    "https://jayvir101.github.io/lightning-resources/wallpaper.png",
-];
+importScripts("https://jayvir101.github.io/required_offline_files.js");
+var CACHE_NAME = "cache-v1";
 
 self.addEventListener('install', event => {
     event.waitUntil((function() {
+        /*
         Games.forEach(function(item,index) {
             if(item.Offline) {
                 REQUIRED_FILES.push(item.Link);
@@ -32,6 +13,7 @@ self.addEventListener('install', event => {
                 REQUIRED_FILES.push(item.Icon);
             }
         });
+        */
         caches.open(CACHE_NAME).then(function(cache) {
             return cache.addAll(REQUIRED_FILES);
         }).then(() => self.skipWaiting())
