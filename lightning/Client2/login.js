998795e97981c277cbb04b1a1ee3d6056d4593c4
Jayvir Singh <610039964@musd.net>
2018-02-15 11:08:31 -0800

    update

--- lightning/Client2/login.js
+++ lightning/Client2/login.js
@@ -1,17 +1,15 @@
-(function() {
-    var email = document.getElementById("email");
-    var password = document.getElementById("password");
-    var error_message = document.getElementById("error_message");
+var email = document.getElementById("email");
+var password = document.getElementById("password");
+var error_message = document.getElementById("error_message");
 
-    function login() {
-      Server.login(email.value,password.value).catch(function(error) {
-        error_message.innerHTML = error.message;
-      });
+function login() {
+  Server.login(email.value,password.value).catch(function(error) {
+    error_message.innerHTML = error.message;
+  });
+}
+
+Server.onauth(function(user) {
+    if(user) {
+        location.href = "index.html";
     }
-
-    Server.onauth(function(user) {
-        if(user) {
-            location.href = "index.html";
-        }
-    });
-})();
+});
