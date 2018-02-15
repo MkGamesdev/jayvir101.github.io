69099f956ccc3a871c0a7075301f9db2a258be92
Jayvir Singh <610039964@musd.net>
2018-02-14 20:10:42 -0800

    update

--- lightning/index.js
+++ lightning/index.js
@@ -9,19 +9,21 @@ Games.sort(function(a,b) {
     return 0;
 });
 
-if(navigator.appVersion.indexOf("Edge") != -1) {
-    alert("You use Microsoft Edge. That´s no good.");
-    document.body.innerHTML = null;
-    throw new Error("You use IE. That´s no good.");
-};
-
 var Columns = 8;
-
+var Online = navigator.onLine;
 Games.forEach(function(item,index) {
   var Icon_Holder = document.createElement("div");
   var Icon_Img = document.createElement("img");
   var Icon_Title = document.createElement("div");
-  Games_Add_Click(Icon_Img,item.Title);
+  if(!item.Offline) {
+    Games_Add_Click(Icon_Img,item.Title);
+  }
+  if(Online && item.Offline) {
+    Games_Add_Click(Icon_Img,item.Title);
+  }
+  if(!Online && item.Offline) {
+     Icon_Img.style.filter = "grayscale(100%)";
+  }
   Icon_Holder.id = "Icon_Holder";
   Icon_Holder.setAttribute("data-Game-Title",item.Title);
   Icon_Img.id = "Icon_Img";
@@ -47,37 +49,6 @@ function Games_Add_Click(element,link) {
       });
   });
 }
-
-/*
-function glitch() {
-    var r = (Math.floor(Math.random() * 3) + 1);
-    if(r === 1) {
-        document.getElementById("style").outerHTML = "<style id='style'>* {" + "background:-webkit-linear-gradient(" + (Math.floor(Math.random() * 100) + 1) +  "deg," + '#'+Math.random().toString(16).substr(-6) + "," + '#'+Math.random().toString(16).substr(-6) + ") !important;" + "color:" + '#'+Math.random().toString(16).substr(-6) + " !important}</style>";
-    }
-    if(r === 2) {
-        document.getElementById("style").outerHTML = "<style id='style'>* {" + "background:" + '#'+Math.random().toString(16).substr(-6) + " !important;" + "color:" + '#'+Math.random().toString(16).substr(-6) + " !important}</style>";
-    }
-    if(r === 3) {
-        document.getElementById("style").outerHTML = "<style id='style'>* {" + "background:-webkit-radial-gradient(" + '#'+Math.random().toString(16).substr(-6) + "," + '#'+Math.random().toString(16).substr(-6) + ") !important;" + "color:" + '#'+Math.random().toString(16).substr(-6) + " !important}</style>";
-    }
-}
-var israndomcolor = false;
-var randomcolorInterval;
-window.addEventListener("keydown",function(e) {
-    if(e.keyCode == 32) {
-        if(israndomcolor === false) {
-            randomcolorInterval = setInterval(function() {
-                    glitch();
-            },0);
-            israndomcolor = true;
-        }
-        else if(israndomcolor === true) {
-            clearInterval(randomcolorInterval);
-            israndomcolor = false;
-        }
-    }
-});
-*/
 
 var Icon_Holder = document.querySelectorAll("#Icon_Holder");
 var Search = document.getElementById("Search");
